a15b9830dd111e975f938345f64371f66095d2be
Teddy <note@kakao.com>
2022-05-02 17:09:04 +0900

    [220502]

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,13 +3,13 @@ from fastapi import APIRouter, Depends
 
 from app.repository.user import UserRepository
 from app.model.user import UserRegister
-from app.model.response import response
+from app.model.response import Response, response
 
 router = APIRouter(prefix="/api/auth", tags=["Auth"])
 
 
 @router.post("/register")
-async def register(user: UserRegister, repository: UserRepository = Depends(UserRepository)):
+async def register(user: UserRegister, repository: UserRepository = Depends(UserRepository)) -> Response:
     is_exists = await repository.is_exists(user.email)
 
     if is_exists.value:
@@ -22,7 +22,7 @@ async def register(user: UserRegister, repository: UserRepository = Depends(User
 
 
 @router.get("/{id}")
-async def get(id: int, repository: UserRepository = Depends(UserRepository)):
+async def get(id: int, repository: UserRepository = Depends(UserRepository)) -> Response:
     user = await repository.get(id)
     if user is None:
         return response(status_code=404, message="Not found")
