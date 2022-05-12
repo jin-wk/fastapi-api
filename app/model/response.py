cb16aa88894effdb19f21479fc7e43470c9e0559
Teddy <note@kakao.com>
2022-05-12 16:47:08 +0900

    [220512] fix

--- app/model/response.py
+++ app/model/response.py
@@ -4,14 +4,15 @@ from fastapi.encoders import jsonable_encoder
 from typing import Any
 
 
-
 class Response:
     def __call__(self, status_code: int, detail: str, data: Any | None = None) -> JSONResponse:
         if data is not None:
             data = jsonable_encoder(data)
         return JSONResponse(content=dict(detail=detail, data=data), status_code=status_code)
 
+
 response = Response()
+
 
 class ResponseModel(BaseModel):
     detail: str
